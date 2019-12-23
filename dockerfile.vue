d0f78fed0628c9717ffa2fde3486b0d36297bef1
Thomas yang <168447636@qq.com>
2019-12-23 23:37:40 +0800

    1

---
+++ dockerfile.vue
@@ -0,0 +1,11 @@
+FROM node:8-alpine3.9
+RUN echo  'http://mirrors.ustc.edu.cn/alpine/v3.9/main'> /etc/apk/repositories && \
+    echo  'http://mirrors.ustc.edu.cn/alpine/v3.9/community' >>/etc/apk/repositories && \
+    cat /etc/apk/repositories
+RUN apk add  --no-cache git python python2 && \
+   git clone https://github.com/PanJiaChen/vue-element-admin.git 
+RUN apk add make && \
+   cd vue-element-admin && \
+   npm install --registry=https://registry.npm.taobao.org
+WORKDIR vue-element-admin
+CMD [ '/usr/bin/npm' 'run' 'dev' ]  
