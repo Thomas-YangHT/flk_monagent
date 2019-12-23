48c3a1a94b3a0f4558e5ac57aac120a04feaace9
Thomas yang <168447636@qq.com>
2019-12-23 13:37:49 +0800

    1

--- docker-py-uwsgi-nginx.sh
+++ docker-py-uwsgi-nginx.sh
@@ -1,9 +1,10 @@
 docker rm -f uwsgi
 docker run --name uwsgi \
--v /opt/python/uwsgi:/uwsgi \
 --restart=always \
 --dns=192.168.100.1 \
 -e TZ='Asia/Shanghai' \
 -p 8101:80 \
 -d 192.168.100.71:5000/flk_monagent \
 sh /uwsgi/startpython.sh
+
+#-v /opt/python/uwsgi:/uwsgi \
